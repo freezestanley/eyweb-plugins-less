1a89f2e6fcc460349888f4d2c08615e944314333
stanleyxu <stanleyxu@stanleyxudeMacBook-Pro.local>
2019-11-10 08:34:47 +0800

    Bump version 1.0.3

--- index.js
+++ index.js
@@ -1,14 +1,50 @@
+const path = require('path')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 module.exports = {
     configureWebpack: config => {
-        config
-            .entry('index')
-                .add(path.resolve(cwd, './src/index.js'))
-                .end()
-            .output
-                .path(path.resolve(cwd, './dist'))
-                .filename('[name].[hash:8].js')
-                // .publicPath()
-                .library('other')
-                .libraryTarget('umd')
+        config.module.rules.delete('less')
+        config.module.rule('less')
+            .test(/\.less$/)
+       if (config.mode === 'application') {
+            config.module.rule('less')
+                .use('mini')
+                .loader(MiniCssExtractPlugin.loader).options({
+                hmr: process.env.NODE_ENV === 'development',
+                fallback: {
+                    loader: require.resolve('style-loader'),
+                    options: {
+                        singleton: true
+                    }
                 }
+                }).end()
+        
+        } else {
+            config.module.rule('less')
+                .use('style-loader')
+                .loader('style-loader').end()
+        }
+        config.module.rule('less').use('css')
+            .loader('css-loader')
+            .end()
+            .use('postcss')
+            .loader('postcss-loader')
+            .options({
+                ident: 'postcss',
+                plugins: (loader) => [
+                require('postcss-preset-env')(),
+                require('postcss-normalize')({ forceImport: true }),
+                require('postcss-cssnext')(),
+                require('postcss-import')({ root: loader.resourcePath }),
+                require('postcss-selector-namespace')({ selfSelector: ':namespace', namespace: `` })
+                ]})
+            .end()
+            .use('less')
+            .loader('less-loader')
+            .end()
+
+
+        console.log('========')
+        console.log(config.toString())
+        console.log('========')
+    }
 }
